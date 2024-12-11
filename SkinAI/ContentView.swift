751dcb4ea68aa17ca43154a475c0d59f524be04a
Natalia Chicherova <nschicherova@gmail.com>
2024-12-11 09:52:27 +0100

    Initial Commit

---
+++ SkinAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkinAI
+//
+//  Created by Chicherova Natalia2 on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkinAI/SkinAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinAIApp.swift
+//  SkinAI
+//
+//  Created by Chicherova Natalia2 on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SkinAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
